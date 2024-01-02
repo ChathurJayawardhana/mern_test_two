1f3a2069d875ec31701d35a2099cf7c7c73ac70c
Chathura Jayawardhana <105495683+ChathurJayawardhana@users.noreply.github.com>
2024-01-02 18:06:57 +0530

    ccc

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>chathura jayawardhana</h1>
+      <button>click me</button>
+    </div>
+  );
+}
+
+export default App;
