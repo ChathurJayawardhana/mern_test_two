7fdc0901148d3b76a217063b1f8af1c9a01ceb4f
Chathura Jayawardhana <105495683+ChathurJayawardhana@users.noreply.github.com>
2024-01-03 19:02:29 +0530

    save

---
+++ frontend/src/UserForm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Grid} from "@mui/material";
+
+export default function UserForm() {
+  return (
+    <Grid container spacing={2} 
+    sx={{backgroundColor:"#ffff"}}
+    > 
+
+    </Grid>
+  )
+}
