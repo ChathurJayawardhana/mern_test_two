f0445ffcbcd4d74061484aa9139aa4d5a51c3bbf
Chathura Jayawardhana <105495683+ChathurJayawardhana@users.noreply.github.com>
2024-01-07 18:04:19 +0530

    backend

--- backend/app.js
+++ backend/app.js
@@ -14,9 +14,30 @@ app.use(
 app.use(express.json());
 
 app.get('/users',(req,res)=>{
-    controller.getUsers(users => {
-        res.send(users);
+    controller.getUsers((req,res,next) => {
+        res.send();
     });
 })
+
+app.post('/createuser',(req,res)=>{
+   
+    controller.addUser(req.body,(calback) =>{
+        res.send();
+    })
+});
+
+app.post('/updateuser',(req,res)=>{
+   
+    controller.updateUser(req.body,(calback) =>{
+        res.send();
+    })
+});
+
+app.delete('/deleteuser',(req,res)=>{
+   
+    controller.updateUser(req.body,(calback) =>{
+        res.send();
+    })
+});
 
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,28 @@
-const app = require('./app');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const router = require('./router')
+
+
+app.use(cors());
+app.use(express.json());
+
+const uri = 'mongodb+srv://thara:thara@thara.g1bvjsn.mongodb.net/?retryWrites=true&w=majority'
+
+const connect = async () =>{
+    try {
+        await mongoose.connect(uri);
+        console.log('conect to database');
+    } catch (error) {
+        console.log('error',error);
+    }
+}
+
+connect();
 
 const server = app.listen(3001,'127.0.0.1',()=>{
     console.log(`node serve is listining to ${server.address().port}`)
 })
+
+app.use('/api',router)
