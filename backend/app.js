d7204f97eb710e57c944f41384ee40319507dd47
Chathura Jayawardhana <105495683+ChathurJayawardhana@users.noreply.github.com>
2024-01-07 08:24:14 +0530

    cc

---
+++ backend/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const controller = require('./controller')
+
+app.use(cors());
+
+app.use(
+    express.urlencoded({
+        extended:true,
+    })
+);
+
+app.use(express.json());
+
+app.get('/users',(req,res)=>{
+    controller.getUsers(users => {
+        res.send(users);
+    });
+})
+
+module.exports = app;
