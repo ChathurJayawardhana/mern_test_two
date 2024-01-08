2d0b4139b629a483d9cf87c023eb77bc1eb81f4f
Chathura Jayawardhana <105495683+ChathurJayawardhana@users.noreply.github.com>
2024-01-08 10:16:41 +0530

    api call

--- frontend/src/Users.js
+++ frontend/src/Users.js
@@ -1,20 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import UserForm from './UserForm'
 import UsersTable from './UsersTable'
 import { Box } from '@mui/material'
 import axios from 'axios'
 
-const users = [
-  {
-    id:1,
-    name:"chathura"
-  },
-  {
-    id:2,
-    name:"kalani"
-  }
-]
+
 export default function Users() {
+
+  const [users,setUsers] = useState([]);
+  
+   useEffect(()=>{
+      getUsers();
+   },[])
+
+  const getUsers = () => {
+    axios.get('http://localhost:3001/api/users')
+    .then(response => {
+      setUsers(response.data.response);
+    })
+    .catch(error => {
+      console.error(error);
+    })
+  }
+
   return (
     <Box
     sx={{
